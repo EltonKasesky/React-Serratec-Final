e9327ece82653cc05d0eccc92b42bb8cab0ebb50
Patricia Sanches <pfasanches@gmail.com>
2025-06-12 11:58:53 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/routes/AppRouter'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <div>
         <BrowserRouter>
             <AppRouter/>
+            <Header/>
         </BrowserRouter>
     </div>
   )
