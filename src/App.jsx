5878615d662b4b2883e87cb447ffc4f80247818c
EltonKasesky <eltonkasesky9@gmail.com>
2025-06-13 18:56:47 -0300

    Fix header and general app

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,9 @@ import AppRouter from './routes/AppRouter';
 import Footer from './components/Footer/footer';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <main style={{ flex: 1 }}>
-          <AppRouter />
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/global.css"
 import App from './App'
-import Footer from './components/Footer/footer';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
