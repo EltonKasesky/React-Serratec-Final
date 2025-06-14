92897d05ef68d1b0406322f2f10f26140f9b11c9
EltonKasesky <eltonkasesky9@gmail.com>
2025-06-13 21:51:03 -0300

    Create private routes

---
+++ src/pages/ProdutoAdmin/index.jsx
@@ -0,0 +1,11 @@
+import ProdutoAdmin from "../../components/ProdutoAdmin";
+
+const ProdutoAdminPage = () => {
+    return (
+        <>
+            <ProdutoAdmin/>
+        </>
+    );
+}
+
+export default ProdutoAdminPage;
---
+++ src/pages/Unauthorized/index.jsx
@@ -0,0 +1,11 @@
+import Unauthorized from "../../components/Unauthorized";
+
+const UnauthorizedPage = () => {
+    return (
+        <>
+            <Unauthorized/>
+        </>
+    );
+}
+
+export default UnauthorizedPage;
