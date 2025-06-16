c23f16f70cccbebdf2a7d3547b73e8835c0d4302
Caroline Brand <carolbrandb@gmail.com>
2025-06-16 17:27:31 -0300

    Registro dark

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,4 +1,15 @@
-import * as styles from "./Register.module.css"
+import { useEffect } from "react";
+import * as styles from "./Register.module.css";
+
+import Header from "../../components/Header";
+import Copyright from "../Copyright";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { useNavigate, Link } from "react-router-dom";
+
 import {
     IoPersonSharp,
     IoLogIn,
@@ -10,15 +21,23 @@ import {
 } from "react-icons/md";
 import { TbLockFilled } from "react-icons/tb";
 import { HiIdentification } from "react-icons/hi2";
-import { Link, useNavigate } from "react-router-dom";
-import Copyright from "../Copyright";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+
+import { useTheme } from "../../context/ThemeContext";
 import axios from "axios";
-import * as yup from "yup"
+
+document.documentElement.setAttribute('native-dark-active', '');
 
 const Register = () => {
-    const validationPost = yup.object().shape({
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        document.documentElement.classList.add('dark');
+        return () => {
+            document.documentElement.classList.remove('dark');
+        };
+    }, []);
+
+    const validationSchema = yup.object().shape({
         nome: yup.string().required("O nome deve ser preenchido"),
         telefone: yup.string().required("O telefone deve ser preenchido"),
         cpf: yup.string().required("O CPF deve ser preenchido"),
@@ -31,120 +50,107 @@ const Register = () => {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm({ resolver: yupResolver(validationPost) });
+    } = useForm({ resolver: yupResolver(validationSchema) });
 
-    const registerUser = (data) => {
-        axios.post("http://localhost:8080/clientes", data)
-            .then(() => {
-                useNavigate("/login")
-            }).catch((error) => {
-                console.error("Erro ao enviar requisição: ", error)
-            })
-    }
+    const registerUser = async (data) => {
+        try {
+            await axios.post("http://localhost:8080/clientes", data);
+            navigate("/login");
+        } catch (error) {
+            console.error("Erro ao registrar usuário:", error);
+        }
+    };
 
     return (
-        <main className={`${styles.container} ${styles.themeLocal}`}>
-            <div className={styles.primarySection}>
-                <section className={styles.leftSection}>
-                    <Link to={"/"} className={styles.backButton}>
-                        <IoArrowBackCircle className={styles.backIcon} />
-                    </Link>
-                    <h1>Fazer Registro</h1>
-                    <div className={styles.contentForm}>
-                        <form onSubmit={handleSubmit(registerUser)}>
-                            <div className={styles.inputForm}>
-                                <IoPersonSharp className={styles.icon} />
-                                <input
-                                    type="text"
-                                    name="nome"
-                                    id="nome"
-                                    placeholder="Seu nome..."
-                                    {...register('nome')}
-                                    required
-                                />
-                                <p className={yup.errorMessage}>{errors.nome?.message}</p>
-                            </div>
+        <>
+            <Header />
 
-                            <div className={styles.inputForm}>
-                                <MdPermPhoneMsg className={styles.icon} />
-                                <input
-                                    type="tel"
-                                    name="telefone"
-                                    id="telefone"
-                                    placeholder="Seu telefone. Ex: (xx)xxxxx-xxxx"
-                                    {...register('telefone')}
-                                    required
-                                />
-                                <p className={yup.errorMessage}>{errors.telefone?.message}</p>
-                            </div>
+            <main className={`${styles.container} ${styles.themeLocal}`}>
+                <div className={styles.primarySection}>
+                    <section className={styles.leftSection}>
+                        <Link to={"/"} className={styles.backButton}>
+                            <IoArrowBackCircle className={styles.backIcon} />
+                        </Link>
+                        <h1>Fazer Registro</h1>
+                        <div className={styles.contentForm}>
+                            <form onSubmit={handleSubmit(registerUser)}>
+                                <div className={styles.inputForm}>
+                                    <IoPersonSharp className={styles.icon} />
+                                    <input
+                                        type="text"
+                                        placeholder="Seu nome..."
+                                        {...register('nome')}
+                                    />
+                                    <p className={styles.errorMessage}>{errors.nome?.message}</p>
+                                </div>
 
-                            <div className={styles.inputForm}>
-                                <HiIdentification className={styles.icon} />
-                                <input
-                                    type="text"
-                                    name="cpf"
-                                    id="cpf"
-                                    placeholder="Seu CPF. Ex: xxx.xxx.xxx-xx"
-                                    {...register('cpf')}
-                                    required
-                                />
-                                <p className={yup.errorMessage}>{errors.cpf?.message}</p>
-                            </div>
+                                <div className={styles.inputForm}>
+                                    <MdPermPhoneMsg className={styles.icon} />
+                                    <input
+                                        type="tel"
+                                        placeholder="Seu telefone. Ex: (xx)xxxxx-xxxx"
+                                        {...register('telefone')}
+                                    />
+                                    <p className={styles.errorMessage}>{errors.telefone?.message}</p>
+                                </div>
 
-                            <div className={styles.inputForm}>
-                                <HiIdentification className={styles.icon} />
-                                <input
-                                    type="text"
-                                    name="cep"
-                                    id="cep"
-                                    placeholder="Seu CEP. Ex: xxxxx-xxx"
-                                    {...register('cep')}
-                                    required
-                                />
-                                <p className={yup.errorMessage}>{errors.cep?.message}</p>
-                            </div>
+                                <div className={styles.inputForm}>
+                                    <HiIdentification className={styles.icon} />
+                                    <input
+                                        type="text"
+                                        placeholder="Seu CPF. Ex: xxx.xxx.xxx-xx"
+                                        {...register('cpf')}
+                                    />
+                                    <p className={styles.errorMessage}>{errors.cpf?.message}</p>
+                                </div>
 
-                            <div className={styles.inputForm}>
-                                <MdEmail className={styles.icon} />
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    placeholder="Seu email. Ex: farmacia@gmail.com"
-                                    {...register('email')}
-                                    required
-                                />
-                                <p className={yup.errorMessage}>{errors.email?.message}</p>
-                            </div>
+                                <div className={styles.inputForm}>
+                                    <HiIdentification className={styles.icon} />
+                                    <input
+                                        type="text"
+                                        placeholder="Seu CEP. Ex: xxxxx-xxx"
+                                        {...register('cep')}
+                                    />
+                                    <p className={styles.errorMessage}>{errors.cep?.message}</p>
+                                </div>
 
-                            <div className={styles.inputForm}>
-                                <TbLockFilled className={styles.icon} />
-                                <input
-                                    type="password"
-                                    name="senha"
-                                    id="senha"
-                                    placeholder="Sua senha..."
-                                    {...register('senha')}
-                                    required
-                                />
-                                <p className={yup.errorMessage}>{errors.senha?.message}</p>
-                            </div>
+                                <div className={styles.inputForm}>
+                                    <MdEmail className={styles.icon} />
+                                    <input
+                                        type="email"
+                                        placeholder="Seu email. Ex: farmacia@gmail.com"
+                                        {...register('email')}
+                                    />
+                                    <p className={styles.errorMessage}>{errors.email?.message}</p>
+                                </div>
 
-                            <button className={styles.btn} type="submit">Registrar</button>
-                        </form>
+                                <div className={styles.inputForm}>
+                                    <TbLockFilled className={styles.icon} />
+                                    <input
+                                        type="password"
+                                        placeholder="Sua senha..."
+                                        {...register('senha')}
+                                    />
+                                    <p className={styles.errorMessage}>{errors.senha?.message}</p>
+                                </div>
 
-                        <p className={styles.userLogin}>
-                            Eu já sou um usuário <Link to={"/login"}>Fazer login</Link>
-                            <IoLogIn className={styles.icon} />
-                        </p>
-                    </div>
-                </section>
-                <section className={styles.rightSection}>
-                    <div className={styles.loginImage}></div>
-                </section>
-            </div>
-            <Copyright />
-        </main>
+                                <button className={styles.btn} type="submit">Registrar</button>
+                            </form>
+
+                            <p className={styles.userLogin}>
+                                Eu já sou um usuário <Link to={"/login"}>Fazer login</Link>
+                                <IoLogIn className={styles.icon} />
+                            </p>
+                        </div>
+                    </section>
+                    <section className={styles.rightSection}>
+                        <div className={styles.loginImage}></div>
+                    </section>
+                </div>
+
+                <Copyright />
+            </main>
+        </>
     );
 };
 
