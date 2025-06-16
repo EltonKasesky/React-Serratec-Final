b74c274b5deb28063d61de8ff94ca9c8a0d1ba9e
Caroline Brand <carolbrandb@gmail.com>
2025-06-15 21:04:15 -0300

    Logo sem fundo, ajuste css do post e estrutura para o sistema receber o dakrmode, que esta parcialmente estruturado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <AuthProvider>
-                <AppRouter />
-            </AuthProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <AppRouter />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
